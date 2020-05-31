b78464865f17028dbe25ace6cb7cb959e42832f5
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-05-31 21:48:26 +0200

    cest juste ouf

--- c4.py
+++ c4.py
@@ -46,7 +46,7 @@ def period(rational):
 # print(isRepeating('14285', '1428571428571428'))     #False
 
 
-def fm(number):
+def fm(number, exactValue=True):
     number = float(number)  #check if its a number
 
     #if its a whole number, get rid of the decimal part
@@ -61,16 +61,24 @@ def fm(number):
     # we consider its a non-decimal if it has at least 10 digits
     if(len(str(number)) >= 10):
         decParts = period(number)  #0 -> fixed, 1 -> periodic part
-        print(decParts) #debug
+        # print(decParts) #debug
+
+        if not exactValue:
+            return ent + '.' + decParts[0] + '[' + decParts[1] + ']..'
+
         a = int(decParts[1])
         b = int(len(decParts[1]) * '9') * 10 ** len(decParts[0])
         if decParts[0] != '':
             a += int(decParts[0]) * int(len(decParts[1]) * '9')
-
         a += int(ent) * b   #la partie entiere
+
+        
 
     # else if its a decimal..
     else:
+        if not exactValue:
+            return ent + '.' + dec
+
         a = int(dec) + int(ent) * 10 * len(dec)
         b = int('1' + len(dec) * '0') 
 
@@ -80,55 +88,55 @@ def fm(number):
 
     # evaluate if its better to return the fraction or the raw number
     frac = '(' + str(a) + '/' + str(b) + ')'
-    # if len(frac) < 8 or len(frac) < len(str(number)):
-    return frac
+    if len(frac) < 8 or len(frac) < len(str(number)):
+        return frac
     
     return str(number)
 
-print(fm(0.0419971997199))
+# print(fm(0.0419971997199))
 
-# def genSpaces(string):
-#     string = str(string)
+def genSpaces(string):
+    string = str(string)
 
-#     if string.isnumeric():
-#         return int(string) * ' '
+    if string.isnumeric():
+        return int(string) * ' '
     
-#     return len(string) * ' '
+    return len(string) * ' '
 
 
-# inputText = \
-# '''
-# [a]ddition
-# [s]ubstraction
-# [m]ultiplication
-# [d]ivision
-# >'''
+inputText = \
+'''
+[a]ddition
+[s]ubstraction
+[m]ultiplication
+[d]ivision
+>'''
 
-# print('__Simple calculator__')
-# userInput=''
+print('__Simple calculator__')
+userInput=''
 
-# while userInput not in ['a', 's', 'm', 'd']:
-#     userInput = input(inputText)[0]
+while userInput not in ['a', 's', 'm', 'd']:
+    userInput = input(inputText)[0]
 
-# first = float(input('Enter a first operand : '))
-# second = float(input('Enter a second operand : '))
+first = float(input('Enter a first operand : '))
+second = float(input('Enter a second operand : '))
 
-# if userInput == 'a':
-#     op = fm(first) + ' + ' + fm(second)
-#     res = first + second
+if userInput == 'a':
+    op = fm(first) + ' + ' + fm(second)
+    res = first + second
 
-# elif userInput == 's':
-#     op = fm(first) + ' - ' + fm(second)
-#     res = first - second
+elif userInput == 's':
+    op = fm(first) + ' - ' + fm(second)
+    res = first - second
 
-# elif userInput == 'm':
-#     op = fm(first) + ' x ' + fm(second)
-#     res = first * second
+elif userInput == 'm':
+    op = fm(first) + ' x ' + fm(second)
+    res = first * second
 
-# elif userInput == 'd':
-#     op = fm(first) + ' / ' + fm(second)
-#     res = first / second
+elif userInput == 'd':
+    op = fm(first) + ' / ' + fm(second)
+    res = first / second
 
-# print(op, '=', fm(res))
-# if(not res.is_integer() and fm(res) != str(res)):
-#     print(genSpaces(op), '=', res)
+print(op, '=', fm(res))
+if(not res.is_integer() and fm(res) != str(res)):
+    print(genSpaces(op), '=', fm(res, False))
